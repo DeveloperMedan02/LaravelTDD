53a49672dd0738a4dcc50691e42ae5dd2f85091f
Ali Agus Hutapea <aliagus@nusa.net.id>
2020-04-20 11:15:18 +0700

    Penambahan tools scout laravel

--- app/Employee.php
+++ app/Employee.php
@@ -5,9 +5,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Http\Resources\Employee as EmployeeResource;
+use Laravel\Scout\Searchable;
 
 class Employee extends Model
 {
+    use Searchable;
+
     protected $fillable = ['name', 'email', 'address', 'hp', 'city', 'province'];
 
     public function childrens(){
